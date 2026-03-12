21a64d71ce254878908b89a61a700c829376dcb1
Daniel Gomes <dannyelgjl@gmail.com>
2026-03-11 23:47:21 -0300

    feat: creating formats end adjustments theme

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,8 @@
+export const SECURE_KEYS = {
+  PIN_VALUE: 'wallet_pin_value',
+};
+
+export const SESSION_TIMEOUT_MS = 2 * 60 * 1000;
+export const SESSION_CHECK_INTERVAL_MS = 5 * 1000;
+export const WALLET_SYNC_INTERVAL_MS = 15 * 1000;
+export const WALLET_SNAPSHOT_CACHE_KEY = '@wallet/snapshot';
